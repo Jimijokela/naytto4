9d6f8febbacd97ab94da4a4861ee3975ee44e003
EetuDoesCoding <eetuelo@gmail.com>
2017-02-23 13:43:34 +0200

    23.2

---
+++ etusivu.php
@@ -0,0 +1,171 @@
+
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<title>Foundation | Welcome</title>
+<link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel='stylesheet' type='text/css'>
+ 
+</head>
+<body>
+ 
+ 
+ 
+<script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-2195009-2', 'auto');
+      ga('send', 'pageview');
+
+      ga('create', 'UA-2195009-27', 'auto', {name: "foundation"});
+      ga('foundation.send', 'pageview');
+
+    </script>
+<style>.fi-social-facebook{color:dodgerblue;font-size:2rem;}.fi-social-youtube{color:red;font-size:2rem;}.fi-social-pinterest{color:darkred;font-size:2rem;}i.fi-social-instagram{color:brown;font-size:2rem;}i.fi-social-tumblr{color:navy;font-size:2rem;}.fi-social-twitter{color:skyblue;font-size:2rem;}</style>
+<header>
+ 
+<div class="top-bar">
+<div class="top-bar-left">
+<ul class="menu">
+<li><a href="#">One</a></li>
+<li><a href="#">Two</a></li>
+<li><a href="#">Three</a></li>
+<li><a href="#">Four</a></li>
+</ul>
+</div>
+<div class="top-bar-right">
+<ul class="menu">
+<li><input type="search" placeholder="Search"></li>
+<li><button type="button" class="button">Search</button></li>
+</ul>
+</div>
+</div>
+ 
+ 
+<div class="row">
+<div class="medium-4 columns">
+<img src="https://placehold.it/450x183&text=LOGO" alt="company logo">
+</div>
+<div class="medium-8 columns">
+<img src="https://placehold.it/900x175&text=Responsive Ads - ZURB Playground/333" alt="advertisement for deep fried Twinkies">
+</div>
+</div>
+ 
+<br>
+<div class="title-bar" data-responsive-toggle="main-menu" data-hide-for="medium">
+<button class="menu-icon" type="button" data-toggle></button>
+<div class="title-bar-title">Menu</div>
+</div>
+<div class="top-bar" id="main-menu">
+<ul class="menu vertical medium-horizontal expanded medium-text-center" data-responsive-menu="drilldown medium-dropdown">
+<li class="has-submenu"><a href="#">Tech</a>
+<ul class="submenu menu vertical" data-submenu>
+<li><a href="#">One</a></li>
+<li><a href="#">Two</a></li>
+<li><a href="#">Three</a></li>
+</ul>
+</li>
+<li class="has-submenu"><a href="#">Energy</a>
+<ul class="submenu menu vertical" data-submenu>
+<li><a href="#">One</a></li>
+<li><a href="#">Two</a></li>
+<li><a href="#">Three</a></li>
+</ul>
+</li>
+<li class="has-submenu"><a href="#">Space</a>
+<ul class="submenu menu vertical" data-submenu>
+<li><a href="#">One</a></li>
+<li><a href="#">Two</a></li>
+<li><a href="#">Three</a></li>
+</ul>
+</li>
+<li class="has-submenu"><a href="#">Medicine</a>
+<ul class="submenu menu vertical" data-submenu>
+<li><a href="#">One</a></li>
+<li><a href="#">Two</a></li>
+<li><a href="#">Three</a></li>
+</ul>
+</li>
+<li class="has-submenu"><a href="#">Robotics</a>
+<ul class="submenu menu vertical" data-submenu>
+<li><a href="#">One</a></li>
+<li><a href="#">Two</a></li>
+<li><a href="#">Three</a></li>
+</ul>
+</li>
+<li class="has-submenu"><a href="#">Tesla</a>
+<ul class="submenu menu vertical" data-submenu>
+<li><a href="#">One</a></li>
+<li><a href="#">Two</a></li>
+<li><a href="#">Three</a></li>
+</ul>
+</li>
+</ul>
+</div>
+</header>
+
+<article>
+<br><br><br>
+</article>
+
+</div>
+</div>
+<footer>
+<div class="row expanded callout secondary">
+<div class="large-4 columns">
+<h5>FLICKR IMAGES</h5>
+<div class="row small-up-4">
+<div class="column"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+<div class="column"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+<div class="column"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+<div class="column"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+<div class="column"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+<div class="column"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+<div class="column"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+<div class="column"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+</div>
+</div>
+<div class="large-4 columns">
+<h5>FLICKR IMAGES</h5>
+<span class="secondary label">Space</span>
+<span class="secondary label">Galaxies</span>
+<span class="secondary label">Milky Way</span>
+<span class="secondary label">Black Holes</span>
+<span class="secondary label">Rebels</span>
+<span class="secondary label">Death Star</span>
+<span class="secondary label">Republic</span>
+<span class="secondary label">R2D2</span>
+</div>
+<div class="large-4 columns">
+<h5>RANDOM MAG</h5>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti quam voluptatum vel repellat ab similique molestias molestiae ea omnis eos, id asperiores est praesentium, voluptate officia nulla aspernatur sequi aliquam.</p>
+</div>
+</div>
+<div class="row expanded">
+<div class="medium-6 columns">
+<ul class="menu">
+<li><a href="#">Legal</a></li>
+<li><a href="#">Partner</a></li>
+<li><a href="#">Explore</a></li>
+</ul>
+</div>
+<div class="medium-6 columns">
+<ul class="menu align-right">
+<li class="menu-text">Copyright © 2099 Random Mag</li>
+</ul>
+</div>
+</div>
+</footer>
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+<script>
+      $(document).foundation();
+    </script>
+<script type="text/javascript" src="https://intercom.zurb.com/scripts/zcom.js"></script>
+</body>
+</html>
